0a74b7ef8d1ad0cae334ae5e76c4faef8539aa25
Daniele Trapani <daniele.trapani@outlook.it>
2023-12-01 21:05:43 +0100

    New backup 2023-12-01 21:05:43

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,3 +8,29 @@ vim.api.nvim_set_keymap("n", "<C-h>", ":TmuxNavigateLeft<CR>", { noremap = true,
 vim.api.nvim_set_keymap("n", "<C-j>", ":TmuxNavigateDown<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-k>", ":TmuxNavigateUp<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-l>", ":TmuxNavigateRight<CR>", { noremap = true, silent = true })
+
+-- harpoon
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hh",
+  ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>ha",
+  ":lua require('harpoon.mark').add_file()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hn",
+  ":lua require('harpoon.ui').nav_next()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>hp",
+  ":lua require('harpoon.ui').nav_prev()<CR>",
+  { noremap = true, silent = true }
+)
