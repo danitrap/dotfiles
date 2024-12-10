87a1efe8dc6d31d4e3628440908b94fc4c32e0a2
Daniele Trapani <daniele.trapani@facile.it>
2024-12-10 17:39:00 +0100

    add rule to execute vitest specs only in __tests__ folder

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -4,10 +4,16 @@ return {
     dependencies = {
       "marilari88/neotest-vitest",
     },
-    opts = {
-      adapters = {
-        ["neotest-vitest"] = {},
-      },
-    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-vitest")({
+          is_test_file = function(file_path)
+            -- only run tests in __tests__ directory
+            return string.match(file_path, "__tests__")
+          end,
+        })
+      )
+    end,
   },
 }
