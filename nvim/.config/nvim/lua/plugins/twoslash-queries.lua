93a482589edcc5d1784f713057a91fdf9be6e0bd
Daniele Trapani <daniele.trapani@outlook.it>
2024-12-15 15:29:35 +0100

    add ubuntu conf

--- nvim/.config/nvim/lua/plugins/twoslash-queries.lua
+++ nvim/.config/nvim/lua/plugins/twoslash-queries.lua
@@ -1,4 +1,18 @@
 return {
-  "marilari88/twoslash-queries.nvim",
-  ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  {
+    "marilari88/twoslash-queries.nvim",
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        opts = function(_, opts)
+          local attach = function(client, bufnr)
+            require("twoslash-queries").attach(client, bufnr)
+          end
+          opts["servers"]["vtsls"]["on_attach"] = attach
+          opts["servers"]["tsserver"]["on_attach"] = attach
+        end,
+      },
+    },
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  },
 }
